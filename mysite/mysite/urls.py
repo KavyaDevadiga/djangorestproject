2aeda23bea052d618bb8ef40afac4fb756399374
KavyaDevadiga <kdevadiga74@gmail.com>
2022-01-11 18:06:57 +0530

    project

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,32 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from books import views as view
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('', view.api_root),
+    path('admin/', admin.site.urls),
+    path('books/',view.book_list.as_view(),name='book-list'),
+    path('books/<int:pk>/',view.book_detail.as_view(),name='book-detail'),
+    path('users/', view.UserList.as_view(),name="user-list"),
+    path('users/<int:pk>/', view.UserDetail.as_view(),name="user-detail"),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
